28aa94e44548cb51dfbfccb49857f4dda2e5f68d
Art Karasev <as.karasyov@gmail.com>
2023-08-07 22:50:54 +1200

    hotfix

--- src/Forms/GridField/GridFieldDetailFormItemRequestExtension.php
+++ src/Forms/GridField/GridFieldDetailFormItemRequestExtension.php
@@ -10,7 +10,9 @@ class GridFieldDetailFormItemRequestExtension extends Extension
     public function updateFormActions($actions)
     {
         // $actions->fieldByName('RightGroup')->addExtraClass('TESTO');
-
-        $actions->insertBefore('ActionMenus', LiteralField::create('test', '<a target="_blank" href="'.$this->owner->getRecord()->Link().'?stage=Stage" class="btn btn-primary bi bi-binoculars-fill me-1" title="Review page on the website"></a>'));
+        if ($this->owner->getRecord()->hasMethod('Link'))
+        {
+            $actions->insertBefore('ActionMenus', LiteralField::create('test', '<a target="_blank" href="'.$this->owner->getRecord()->Link().'?stage=Stage" class="btn btn-primary bi bi-binoculars-fill me-1" title="Review page on the website"></a>'));
+        }
     }
 }
